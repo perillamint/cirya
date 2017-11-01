dbb10d74986cb920752e8a909a2fd3184aec15d2
perillamint <perillamint@gentoo.moe>
2017-11-02 00:25:51 +0900

    Add discord_alchemy

--- apps/discord_adapter/mix.exs
+++ apps/discord_adapter/mix.exs
@@ -28,6 +28,7 @@ defmodule Hedwig.Adapters.Discord.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
+      {:alchemy, "~> 0.6.0", hex: :discord_alchemy}
     ]
   end
 end
