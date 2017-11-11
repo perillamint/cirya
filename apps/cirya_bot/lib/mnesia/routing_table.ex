e42b1535f73db86d2903e4f70b46df9e05b9db82
perillamint <perillamint@gentoo.moe>
2017-11-12 02:55:35 +0900

    Refactor DB

--- apps/cirya_bot/lib/mnesia/routing_table.ex
+++ apps/cirya_bot/lib/mnesia/routing_table.ex
@@ -2,22 +2,14 @@ require Exquisite
 use Amnesia
 
 defdatabase CiryaBot.Mnesia.RoutingTable do
-  deftable Destination, [{:id, autoincrement}, :svc_name, :room], type: :ordered_set do
-    @type t :: %Destination{
-      id: non_neg_integer,
-      svc_name: atom,
-      room: binary
-    }
-  end
-
-  deftable Source, [
+  deftable Room, [
     {:id, autoincrement},
     :svc_name,
     :room,
     :alias,
     :destinations
   ], type: :ordered_set, index: [:svc_name, :room] do
-    @type t :: %Source{
+    @type t :: %Room{
       id: non_neg_integer,
       svc_name: atom,
       room: binary,
@@ -27,7 +19,7 @@ defdatabase CiryaBot.Mnesia.RoutingTable do
 
     def get_destinations(self) do
       {_, destinations} = Enum.map_reduce(self.destinations, [], fn(destination, acc) ->
-        dest = Destination.read(destination)
+        dest = Room.read(destination)
         {dest, acc ++ [dest]}
       end)
 
