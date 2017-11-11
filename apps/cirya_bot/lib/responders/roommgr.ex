46b2aeeb8af50507dbb16485db3fb15fdef00b8c
perillamint <perillamint@gentoo.moe>
2017-11-12 00:03:45 +0900

    Room management util

---
+++ apps/cirya_bot/lib/responders/roommgr.ex
@@ -0,0 +1,63 @@
+defmodule CiryaBot.Responders.RoomManager do
+  @moduledoc """
+  Room management command provider
+  """
+
+  use Amnesia
+  use Hedwig.Responder
+  require Exquisite
+
+  alias CiryaBot.Mnesia.RoutingTable.Source, as: RouteSrc
+
+  require RouteSrc
+
+  @usage """
+  hedwig setalias <alias> - Set room alias
+  """
+  respond ~r/setalias (?<alias>.*)/, msg do
+    userid = msg.user.id
+    {:ok, svcname} = userid |> String.split("@") |> Enum.fetch(-1)
+    svc = String.to_atom(svcname)
+
+    replymsg = Amnesia.transaction do
+      src = RouteSrc.where(svc_name == svc and room == msg.room) |> Amnesia.Selection.values
+      case src do
+        [] ->
+          "No room found on DB. Cannot set alias."
+        [entry] ->
+          %{entry|alias: msg.matches["alias"]} |> RouteSrc.write
+          "Room found on DB. Aliased as: " <> msg.matches["alias"]
+      end
+    end
+
+    reply msg, replymsg
+  end
+
+  @usage """
+  hedwig createroom - Create room on DB
+  """
+  respond ~r/createroom$/i, msg do
+    userid = msg.user.id
+    {:ok, svcname} = userid |> String.split("@") |> Enum.fetch(-1)
+    svc = String.to_atom(svcname)
+
+    replymsg = Amnesia.transaction do
+      src = RouteSrc.where(svc_name == svc and room == msg.room) |> Amnesia.Selection.values
+
+      case src do
+        [] ->
+          %RouteSrc{
+            svc_name: svc,
+            room: msg.room,
+            alias: nil,
+            destinations: []
+          } |> RouteSrc.write()
+          "Room created."
+        [entry] ->
+          "Room already exists!"
+      end
+    end
+
+    reply msg, replymsg
+  end
+end
